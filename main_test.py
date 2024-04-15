13d08b0bacc51dd399d5d167868385592537bbb3
Redstoneur <alfa.aog36@gmail.com>
2024-04-15 15:11:01 +0200

    Update main_test.py
    Code comment in English

--- main_test.py
+++ main_test.py
@@ -1,43 +1,59 @@
+###############################################################################################
+##### Import ##################################################################################
+###############################################################################################
+
 import unittest
+
 import main
 
-# Classe de test pour la classe Debug
+
+###############################################################################################
+##### Classes #################################################################################
+###############################################################################################
+
+# Test class for the Debug class
 class TestDebug(unittest.TestCase):
-    # Test pour la méthode __call__
+    # Test for the __call__ method
     def test_call(self):
         debug = main.Debug(True)
-        # La méthode __call__ ne retourne rien, donc on vérifie si elle est None
+        # The __call__ method returns nothing, so we check if it is None
         self.assertIsNone(debug("Test message"))
 
-    # Test pour la méthode __str__
+    # Test for the __str__ method
     def test_str(self):
         debug = main.Debug(True)
-        # On vérifie si la représentation en chaîne de caractères est correcte
+        # We check if the string representation is correct
         self.assertEqual(str(debug), "debug(True)")
 
-# Classe de test pour la classe Favorite_Link
+
+# Test class for the Favorite_Link class
 class TestFavoriteLink(unittest.TestCase):
-    # Test pour la méthode __str__
+    # Test for the __str__ method
     def test_str(self):
         link = main.Favorite_Link("Title", "URL", "Folder")
         expected_str = "Title: Title\nURL: URL\nFolder: Folder"
-        # On vérifie si la représentation en chaîne de caractères est correcte
+        # We check if the string representation is correct
         self.assertEqual(str(link), expected_str)
 
-    # Test pour la méthode __dict__
+    # Test for the __dict__ method
     def test_dict(self):
         link = main.Favorite_Link("Title", "URL", "Folder")
         expected_dict = {"Title": "Title", "URL": "URL", "Folder": "Folder"}
-        # On vérifie si la représentation en dictionnaire est correcte
+        # We check if the dictionary representation is correct
         self.assertEqual(link.__dict__(), expected_dict)
 
-    # Test pour la méthode __list__
+    # Test for the __list__ method
     def test_list(self):
         link = main.Favorite_Link("Title", "URL", "Folder")
         expected_list = ["Title", "URL", "Folder"]
-        # On vérifie si la représentation en liste est correcte
+        # We check if the list representation is correct
         self.assertEqual(link.__list__(), expected_list)
 
-# Exécution des tests
+
+###############################################################################################
+##### Main ####################################################################################
+###############################################################################################
+
+# Run the tests
 if __name__ == '__main__':
     unittest.main()
